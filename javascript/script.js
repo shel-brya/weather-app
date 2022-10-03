dc09951626f3f2a7d537c12c065f4fa3cbd9168e
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2022-10-03 19:16:45 -0400

    Moved project into folder

---
+++ javascript/script.js
@@ -0,0 +1,89 @@
+// ----- display searched city and current weather -------
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp-display").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+// display default city
+function displayDefaultCity(city) {
+  let apiKey = "3c949ba49d38be2487ee278e0d2d4059";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+//display searched city into header
+function citySearch(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#city-search").value;
+  displayDefaultCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", citySearch);
+
+function dateFormat(date) {
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[now.getDay()];
+
+  return `${day} ${hours}:${mins}`;
+}
+
+// display current date and time
+let now = new Date();
+
+let li = document.querySelector("#date");
+
+li.innerHTML = dateFormat(now);
+
+displayDefaultCity("Bloomington");
+
+// switch between celsius and farh -- I am not sure how to keep the ° when switching
+// let defaultTemp = document.querySelector("#temp-display");
+// defaultTemp.innerHTML = 76;
+
+// function celsiusTemp(event) {
+//   event.preventDefault();
+//   let temp = document.querySelector("#temp-display");
+//   temp.innerHTML = 13;
+// }
+
+// function farhTemp(event) {
+//   event.preventDefault();
+//   let temp = document.querySelector("#temp-display");
+//   temp.innerHTML = 76;
+// }
+
+// let cTemp = document.querySelector("#celsius");
+// cTemp.addEventListener("click", celsiusTemp);
+
+// let fTemp = document.querySelector("#farh");
+// fTemp.addEventListener("click", farhTemp);
