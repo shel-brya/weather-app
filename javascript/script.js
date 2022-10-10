b979fea90adaeb4eefe1db44f03c786576984366
shelbybryant <69205358+shelbybryant@users.noreply.github.com>
2022-10-10 12:35:03 -0500

    linked weather image and alt description with api

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,13 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 // display default city
